a12a48ee4d51ba42af6a65a2f8ef57e62e0cfaaa
KingAnh <58280404+AnhTan1420@users.noreply.github.com>
2021-09-13 13:43:50 +0700

    Create ExternalContract.sol

---
+++ ExternalContract.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.0;
+
+contract ExternalContract{
+    function externalCall() external returns(unit){
+        return 123;
+    }
+    function publicCall() public returns(unit){
+        return 123;
+    }
+}
